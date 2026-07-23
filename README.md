5bc7032964b2a31f48f8a73268185f24b167fc4d
MiguelRavagnani <miguel.ravagnani@gmail.com>
2026-07-23 19:05:51 +0200

    WIP: More data model and readme draft

---
+++ README.md
@@ -0,0 +1,13 @@
+# diane
+
+Inspired by Twin Peaks.
+
+## Roadmap
+
+- [x] Define the basic data model for the app
+- [ ] Make sure it compiles (kind of)
+- [ ] Settle for a file format
+- [ ] Write the file parsers, sink adn serializers
+- [ ] Write the capture as a CLI
+- [ ] Start drafting the TUI
+- [ ] TBD
--- src/app.rs
+++ src/app.rs
@@ -55,6 +55,10 @@ pub enum Field {
 pub struct App {
     pub vault: PathBuf,
 
+    // NOTE: This might bite me. If we are loading from disk
+    // 365 notes for a year, for example, this sohuld be ok. But
+    // lazily loading this should be smarter. Maybe move this to
+    // BTreeMap later
     pub days: Vec<Day>,
     pub day_cursor: usize,
 
@@ -82,6 +86,29 @@ impl App {
 
     pub fn today_mut(&mut self) -> &mut Day {
         todo!()
+    }
+
+    pub fn push_record(&mut self, text: String) {
+        let at = chrono::Local::now().time();
+        self.today_mut().records.push(Record { at, text });
+    }
+
+    pub fn commit_pair(&mut self) {
+        // Mode::Browsing could be the default in the enum definition,
+        // but i prefere to have this replace written explicitly.
+        if let Mode::EditingPair { key, value, .. } =
+            std::mem::replace(&mut self.mode, Mode::Browsing)
+        {
+            if key.trim().is_empty() {
+                return;
+            }
+            if let Some(note) = self.selected_note_mut() {
+                match note.frontmatter.iter_mut().find(|(k, _)| *k == key) {
+                    Some(slot) => slot.1 = value,
+                    None => note.frontmatter.push((key, value)),
+                }
+            }
+        }
     }
 
     pub fn selected_note_mut(&mut self) -> Option<&mut Note> {

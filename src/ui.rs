123d48300dfaa92376fe73e08f1691007e88796b
MiguelRavagnani <miguel.ravagnani@gmail.com>
2026-08-06 16:23:24 +0200

    feat: Adding a few comments. Also preparing the scaffolding for the journal records

--- src/ui.rs
+++ src/ui.rs
@@ -113,15 +113,28 @@ fn draw_capture_poopup(frame: &mut Frame, text: &str, character_index: &u16) {
     let area = centered_rect(65, 7, frame.area());
     let diane_theme = Theme::default();
 
+    // Im starting to get lost, so Ill leave some comments here about the
+    // damn UI
+    //
+    // Clears the area behind the to-be-rendered background slpash
     frame.render_widget(Clear, area);
+
+    // Render the splash
     frame.render_widget(Block::default().bg(diane_theme.splash_bg), area);
 
+    // Main block. Will hold the capture popup
     let main_block = Block::bordered()
         .border_type(BorderType::Rounded)
         .border_style(Style::new().fg(diane_theme.border));
+
     let main_block_inner = main_block.inner(area).inner(Margin::new(2, 0));
+
     frame.render_widget(main_block, area);
 
+    // here, the division is:
+    //  1 - Header
+    //  2 - Spacer
+    //  3 - Input area body
     let main_block_inner_rows = Layout::vertical([
         Constraint::Length(1),
         Constraint::Length(1),
@@ -129,6 +142,7 @@ fn draw_capture_poopup(frame: &mut Frame, text: &str, character_index: &u16) {
     ])
     .split(main_block_inner);
 
+    // Spacer hack
     frame.render_widget(
         Block::new()
             .borders(Borders::BOTTOM)
@@ -159,6 +173,7 @@ fn draw_capture_poopup(frame: &mut Frame, text: &str, character_index: &u16) {
     let [input] = Layout::vertical([Constraint::Length(1)])
         .flex(Flex::Center)
         .areas(main_block_inner_rows[2]);
+
     frame.render_widget(
         Paragraph::new(Line::from(vec![
             Span::styled(
@@ -180,6 +195,8 @@ fn draw_journal(frame: &mut Frame) {
     let diane_theme = Theme::default();
 
     frame.render_widget(Clear, area);
+    // NOTE: The slpash might be too much. Maybe Ill remove it for the jorunal
+    // archive window
     frame.render_widget(Block::default().bg(diane_theme.splash_bg), area);
 
     let main_block = Block::bordered()
@@ -190,6 +207,11 @@ fn draw_journal(frame: &mut Frame) {
     frame.render_widget(main_block, area);
 
     let main_block_inner_margin = main_block_inner.inner(Margin::new(2, 0));
+
+    // Area for:
+    //   1 - Header
+    //   2 -Journal/Archive body
+    //   3 - Footer
     let main_block_rows = Layout::vertical([
         Constraint::Length(1),
         Constraint::Min(10),
@@ -217,24 +239,57 @@ fn draw_journal(frame: &mut Frame) {
         main_block_rows[0],
     );
 
-    let workspace_collumns = Layout::horizontal([Constraint::Fill(1), Constraint::Fill(4)])
-        .spacing(Spacing::Overlap(1))
-        .split(main_block_rows[1]);
+    let [workspace_sidepane, workspace_body] =
+        Layout::horizontal([Constraint::Fill(1), Constraint::Fill(4)])
+            .spacing(Spacing::Overlap(1))
+            .areas(main_block_rows[1]);
+
+    let [_, workspace_sidepane_title, workspace_sidepane_content] = Layout::vertical([
+        Constraint::Length(1),
+        Constraint::Length(1),
+        Constraint::Fill(1),
+    ])
+    .areas(workspace_sidepane);
 
     frame.render_widget(
         Block::bordered()
             .borders(Borders::TOP | Borders::BOTTOM | Borders::RIGHT)
             .border_style(Style::new().fg(diane_theme.spacer))
             .merge_borders(MergeStrategy::Exact),
-        workspace_collumns[0],
+        workspace_sidepane,
     );
+
     frame.render_widget(
         Block::bordered()
             .borders(Borders::TOP | Borders::BOTTOM | Borders::LEFT)
             .border_style(Style::new().fg(diane_theme.spacer))
             .merge_borders(MergeStrategy::Exact),
-        workspace_collumns[1],
+        workspace_body,
     );
+
+    frame.render_widget(
+        Paragraph::new(Line::from(Span::styled(
+            "Journal",
+            Style::new().fg(diane_theme.section_title),
+        ))),
+        workspace_sidepane_title,
+    );
+
+    let mock_records = ["Today", "Yesterday"];
+
+    let mock_paragraph = Paragraph::new(
+        mock_records
+            .iter()
+            .map(|&record| {
+                Line::from(vec![
+                    Span::styled("- ", Style::new().fg(diane_theme.info_text)),
+                    Span::styled(record, Style::new().fg(diane_theme.info_text)),
+                ])
+            })
+            .collect::<Vec<_>>(),
+    );
+
+    frame.render_widget(mock_paragraph, workspace_sidepane_content);
 
     frame.render_widget(
         Paragraph::new(
